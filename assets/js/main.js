5cf104286ad673454e71bcfb6f2a342decc2616b
JonyWhalee <jonathandr600@gmail.com>
2023-03-17 18:05:46 -0300

    contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,3 +108,86 @@ let swiper = new Swiper(".portfolio__container",{
       clickeable: true,
   },
 });
+// form
+
+form.addEventListener('submit', e =>{
+  e.preventDefault();
+
+  fetch("https://formsubmit.co/ajax/jonathandr600@gmail.com",{
+      method:'POST',
+      body:new FormData(e.target)
+  })
+  .then(res => res.ok ? res.json():Promise.reject(res))
+  .then(json => {
+      console.log(json);
+      response.innerHTML = `<p>${json.message}</p>`
+      form.reset();
+  })
+  .catch(err => {
+      console.log(err);
+      let message = err.statusText || "please try again";
+      response.innerHTML = `<p>Error ${err.status}: ${message}</p>`
+  })
+  .finally(() => setTimeout(() =>{
+      response.innerHTML = "";
+  }, 3000))
+})
+
+// scroll
+
+const sections = document.querySelectorAll("section[id");
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector(".nav__menu a[href*="+ sectionId + "]").classList.add("active-link");
+        }else{
+            document.querySelector(".nav__menu a[href*="+ sectionId + "]").classList.remove("active-link");
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive);
+
+// background
+
+function scrollHeader(){
+  const nav = document.getElementById("header");
+  if(this.scrollY >= 80) nav.classList.add("scroll-header"); else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+// scroll
+
+function scrollUp(){
+  const scrollUp = document.getElementById("scroll-up");
+  if(this.scrollY >= 560) scrollUp.classList.add("show-scroll"); else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
+
+/*Dark / ligth */
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+if (selectedTheme){
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](iconTheme);
+}
+
+themeButton.addEventListener("click", () =>{
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
+})
